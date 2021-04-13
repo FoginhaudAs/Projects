bc59e4c9654d526b1091961333b7e83e8a94ad24
FoginhaudAs <minhan8646921v@126.com>
2021-04-13 13:39:29 +0800

    ver2

---
+++ Hello2016/Hello2016/MyHello.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2016
+{
+    public class MyHello
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello 2016 小伙伴们大家好！");
+            foreach (string xx in args)
+            {
+                Console.WriteLine("Test {0}", xx);
+            }
+            Console.ResetColor();
+        }
+    }
+}
--- Hello2016/Hello2016/Program.cs
+++ Hello2016/Hello2016/Program.cs
@@ -8,12 +8,6 @@ namespace Hello2016
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Hello 2016 小伙伴们大家好！");
-            Console.ResetColor();
-        }
+        MyHello myhl = new MyHello();
     }
 }
