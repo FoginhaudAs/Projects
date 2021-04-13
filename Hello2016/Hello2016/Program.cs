543a9230471128a508584c5873b61f1a90b2ee21
FoginhaudAs <minhan8646921v@126.com>
2021-04-13 12:57:57 +0800

    ver1

---
+++ Hello2016/Hello2016/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello 2016 小伙伴们大家好！");
+            Console.ResetColor();
+        }
+    }
+}
